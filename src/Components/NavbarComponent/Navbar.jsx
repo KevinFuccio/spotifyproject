ea57ff3c4821922d1a4b4c020898157cb7231ed9
kevin fuccio <kevin.fuccio00@gmai.com>
2023-02-24 12:57:54 +0100

    fiixing icons

---
+++ src/Components/NavbarComponent/Navbar.jsx
@@ -0,0 +1,96 @@
+import { Container, Row } from "react-bootstrap";
+import logo from "../../assets/Spotify_Logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './Navbar.css';
+
+const NavBar = () => {
+  return (
+    <Container>
+      <Row>
+        <div className="col-2">
+          <nav
+            className="navbar navbar-expand-md navbar-white bg-navbar fixed-left justify-content-between"
+            id="sidebar"
+          >
+            <div className="nav-container">
+              <a className="navbar-brand" href="index.html">
+                <img
+                  src={logo}
+                  alt="Spotify_Logo"
+                  width="131"
+                  height="40"
+                />
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <ul>
+                    <li>
+                      <a className="nav-item nav-link" href="index.html">
+                      <FontAwesomeIcon icon="fas fa-home" /> Home
+                      </a>
+                    </li>
+                    <li>
+                      <a className="nav-item nav-link" href="#">
+                      <FontAwesomeIcon icon="fas fa-book-open" /> Your
+                        Library
+                      </a>
+                    </li>
+                    <li>
+                      <div className="input-group mt-3">
+                        <input
+                          type="text"
+                          className="form-control mb-2"
+                          id="searchField"
+                          placeholder="Search"
+                          aria-label="Search"
+                          aria-describedby="basic-addon2"
+                        />
+                        <div
+                          className="input-group-append"
+                          style={{marginBottom: "4%"}}
+                        >
+                          <button
+                            className="btn btn-outline-secondary btn-sm"
+                            type="button"
+                            id="button-addon1"
+                            
+                          >
+                            GO
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="nav-btn">
+              <button className="btn signup-btn" type="button">
+                Sign Up
+              </button>
+              <button className="btn login-btn" type="button">
+                Login
+              </button>  
+            <div>
+                <a href="#">Cookie Policy</a>|<a href="#"> Privacy</a>
+            </div>
+            </div>
+          </nav>
+        </div>
+      </Row>
+    </Container>
+  );
+};
+export default NavBar
